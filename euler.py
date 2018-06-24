3c9ac0c18e1fbcbcc7a973892319e6d3758f70c1
Uberursa <uberursa@gmail.com>
2018-06-24 08:43:02 -0400

    somrthing

--- euler.py
+++ euler.py
@@ -1,4 +1,5 @@
 #I just need a graphing import (matplotlib)
+import matplotlib
 
 def base(x):
 	#this is just the actual equation x^2 for comparison
@@ -6,3 +7,6 @@ def base(x):
 	
 def get_next_val(h, x):
 	return x +(h*(x*2))
+	
+def main():
+	
